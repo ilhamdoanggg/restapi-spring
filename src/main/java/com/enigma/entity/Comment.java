96a7ca1b1f2a2767debfe94063954d19382c2e16
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-10-25 14:01:01 +0700

    add comment

---
+++ src/main/java/com/enigma/entity/Comment.java
@@ -0,0 +1,18 @@
+package com.enigma.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Comment {
+}
+
+
